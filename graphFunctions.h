f070dc802b1f08af2cd235ebd55ca62384887821
pshivam111 <pshivam111@gmail.com>
2020-04-30 12:48:55 +0530

    arranged  codes

--- graphFunctions.h
+++ graphFunctions.h
@@ -6,7 +6,21 @@ Gnode *createGnode(char c)
     temp->Nlist=NULL;
     return  temp;
 }
-
+Gnode *getGadd(char c)
+{
+    // printf("\n Get address function and function parameter is : %c ", c);
+    Gnode *temp=start;
+    
+    while (temp)
+    {
+        // printf("\n in loop %c", temp->data);
+        if(temp->data==c)
+            break;
+        temp=temp->next;
+    }
+    // printf("\n Get address function over and temp data is : %c", temp->data);
+    return temp;
+}
 void addNeighbour(Gnode *a, Gnode *b)
 {
     Nnode *node=(Nnode *)malloc(sizeof(Nnode)),*temp;
@@ -28,6 +42,158 @@ void addNeighbour(Gnode *a, Gnode *b)
     
 }
 
+void filterNeighbour(Gnode *H, Gnode *f)
+{
+    // printf("\n Filtering neighbour started");
+    // printf("\n to be filtered %c",f->data);
+    Nnode *m1,*m2;
+    m1=H->Nlist;
+    if(m1!=NULL)
+    {
+        
+        if(m1->Gadd==f)
+        {
+            H->Nlist=m1->next;
+            free(m1);
+        } 
+        else
+        {  
+            int i=0;    
+            while(1)
+            {
+                //  printf("\n %d",i);
+                
+                m2=m1;
+                m1=m1->next;
+                if(m1==NULL)
+                    break;
+                if(m1->Gadd==f )
+                {
+                    // printf("\nInside if");
+                    m2->next=m1->next;
+                    free(m1);
+                    break;
+                }
+                i++;
+            }
+        }      
+    }    
+}
+
+
+void clearNeighbour(Gnode *node)
+{
+    // printf("\n Clear neighbour function called ");
+    if(node!=NULL)
+    {
+        Nnode *m1,*m2;
+        m1=node->Nlist;
+        while(m1)
+        {
+            m2=m1;
+            m1=m1->next;
+            free(m2);
+        }
+        node->Nlist=NULL;
+    }
+}
+
+void deleteGnode(Gnode *a)
+{
+    // printf("\n Inside Delete node function");
+    Gnode *mover=start,*premover=NULL;
+    
+    // printf("\n%c",mover->data);
+    // printf("\nclearNeighbour started");
+    while(mover!=NULL)
+    {
+        // printf("\n Mover data%c",mover->data);
+        // printf("\nFiltering Neighbour started");
+        if(mover!=a)
+        {
+            // printf("\n other node detected");
+            
+            filterNeighbour(mover,a);
+            // printf("\n filtering over");
+        }
+        premover=mover;
+        mover=mover->next;
+        // printf("\n Next node");
+    }
+    
+    // printf("clearNeighbour done");
+    mover=start;
+    premover=NULL;
+
+    if(mover==a)
+    {
+        start=mover->next;
+        clearNeighbour(mover);
+        free(mover);
+    }
+    else
+    {
+        while(1)
+        {
+            premover=mover;
+            mover=mover->next;
+            if(mover==a)
+            {
+                clearNeighbour(mover);
+                premover->next=mover->next;
+                free(mover);
+                break;
+            }
+        }        
+    }
+    
+    
+}
+
+void deleteNode(char c)
+{
+    deleteGnode(getGadd(c));
+}
+
+
+void deleteEdge(char from, char to)
+{
+    // printf("\n from %c  to %c",from,to );
+    // printf("\n Edge deletion function");
+
+    Gnode *temp=getGadd(from);
+    Gnode *toAdd=getGadd(to);
+    if(temp==NULL)
+        printf(" Address is NULL");
+    // printf("\n Address recieved from getGadd function");
+    // printf("\n temp data %c",temp->data);
+    // printf("\n toAdd data %c",toAdd->data);
+    
+
+    Nnode *mover=temp->Nlist;
+    /*
+    if (mover==NULL)
+        printf("\n Its NULL");
+    else
+        printf("\n Not NULL");
+    */
+    // printf("\n delay");
+    if(mover->next ==NULL)
+    {
+        free(mover); temp->Nlist=NULL;
+    }
+    else
+    {    
+        while (mover->next->Gadd->data!=to)
+        {
+            
+            mover=mover->next;
+        }
+        Nnode *del=mover->next;
+        mover->next=mover->next->next;
+        free(del); 
+    }   
+}
 
 void attachGnode(Gnode *node)
 {
@@ -62,21 +228,7 @@ Gnode *searchGnode(char c)
 }
 
 
-Gnode *getGadd(char c)
-{
-    // printf("\n Get address function and function parameter is : %c ", c);
-    Gnode *temp=start;
-    
-    while (temp)
-    {
-        // printf("\n in loop %c", temp->data);
-        if(temp->data==c)
-            break;
-        temp=temp->next;
-    }
-    // printf("\n Get address function over and temp data is : %c", temp->data);
-    return temp;
-}
+
 void  addEdge(edge e)
 {
     //search for a note with data = e.from
